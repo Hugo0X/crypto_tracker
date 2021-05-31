16ef5c1846810a2c950bbf7240e1ff1ca09a2dbe
Hugo0X <hugo95martin@gmail.com>
2021-05-31 17:33:15 +0200

    fix search mobile, new title

--- src/Form/CryptoType.php
+++ src/Form/CryptoType.php
@@ -6,15 +6,13 @@ use App\Entity\Crypto;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CryptoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            // ->add('name', null, ['label' => false])
-            ->add('name', TextType::class, ['label' => false])
+            ->add('name', null, ['label' => false])
         ;
     }
 
